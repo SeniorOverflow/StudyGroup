bbaca1f587281e09352ed1f2a16fb630910daee9
Igor <37891055+DonEnot@users.noreply.github.com>
2020-04-03 12:12:30 +0500

    MVC start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudyGroup.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
